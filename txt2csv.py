a1e6a85ea511f3f938572f18a783aa84171dbc86
adgarcia <alanadavidgarcia@gmail.com>
2014-01-09 17:36:24 -0500

    Run python txt2csv.py to convert copy pasted lists from range(2001,2014) into csv format (both txt and csv are provided with original data from Master List 2001-2013 by JEREMIAH MOSS). Run again if you add new data to the txt file

---
+++ txt2csv.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import re
+import csv
+
+import sys                                                                  
+reload(sys)
+sys.setdefaultencoding("utf-8")
+
+
+for i in range (2001,2014):
+	year = str(i)
+	readFile = open('txt/'+ year+'.txt','r')
+
+	placetime = []
+	reason = []
+	for i, line in enumerate(readFile):
+		if line != "" and line !="\n":
+			if i%3>0:
+	   			reason.append(line.rstrip())
+	   		elif i%3==0:
+	   			line = re.sub(r'\xe2\x80\x99','\'',line)
+	   			line = [year] + line.split(':')
+	   			line[2] = re.sub(r'[a-zA-Z\s]*','',line[2])
+	   			placetime.append(line)
+
+	for k, v in enumerate(placetime):
+		placetime[k].append(reason[k])
+
+	placetime = [['Closing (Year)','Place','History Lost (Years)','Reason']] + placetime
+
+
+	readFile.close()
+
+	print len(placetime)
+	print placetime
+
+	with open("./csv/"+year+".csv", "wb") as f:
+	    writer = csv.writer(f)
+	    writer.writerows(placetime)
+
