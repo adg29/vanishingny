2bbccb32b37ec8c9833672c63ad2d24c56c94b74
Alan Garcia <alanadavidgarcia@gmail.com>
2014-02-06 17:06:28 -0500

    Parse location data from the description row and add it to the headers and rows

--- txt2csv.py
+++ txt2csv.py
@@ -6,28 +6,38 @@ import sys
 reload(sys)
 sys.setdefaultencoding("utf-8")
 
+aggregate = []
+header = [['Closing (Year)','Place','History Lost (Years)','Reason','Location']] 
 
-for i in range (2000,2014):
+for i in range (2000,2015):
 	year = str(i)
 	readFile = open('txt/'+ year+'.txt','r')
 
 	placetime = []
 	reason = []
+	location = []
 	for i, line in enumerate(readFile):
 		if line != "" and line !="\n":
-			if i%3>0:
-	   			reason.append(line.rstrip())
-	   		elif i%3==0:
+			if i%3>0: # reason and location data 
+				data = line.rstrip().split(';')
+	   			reason.append(data[0])
+	   			if len(data) > 1:
+	   				location.append(data[1]) 
+	   			else:
+	   				location.append('') 
+	   		elif i%3==0: # place and year data
 	   			line = re.sub(r'\xe2\x80\x99','\'',line)
-	   			line = [year] + line.split(':')
+	   			line = [year] + line.split(':') # year integer inlcuded
 	   			line[2] = re.sub(r'[a-zA-Z\s]*','',line[2])
 	   			placetime.append(line)
 
 	for k, v in enumerate(placetime):
 		placetime[k].append(reason[k])
+		placetime[k].append(location[k])
 
-	placetime = [['Closing (Year)','Place','History Lost (Years)','Reason']] + placetime
+	aggregate += placetime
 
+	placetime = [['Closing (Year)','Place','History Lost (Years)','Reason','Location']] + placetime
 
 	readFile.close()
 
@@ -37,4 +47,10 @@ for i in range (2000,2014):
 	with open("./csv/"+year+".csv", "wb") as f:
 	    writer = csv.writer(f)
 	    writer.writerows(placetime)
+
+
+# with open("vanishingny.csv", "wb") as f:
+#     writer = csv.writer(f)
+#     writer.writerows(header + aggregate[::-1])
+
 
